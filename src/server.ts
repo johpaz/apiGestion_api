a78c7873acbc127e49d211b9ca435085b3c8a1ab
johpaz <johpaz252@gmail.com>
2025-11-06 16:31:53 -0500

    feat server

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { Elysia } from 'elysia';
 import { cors } from '@elysiajs/cors';
 import { jwt } from '@elysiajs/jwt';
 import logger from './utils/logger.js';
-import { errorHandler } from './middleware/errorHandler';
+import { errorHandler } from './middleware/errorHandler.js';
 
 // Import routes
 import authRoutes from './routes/auth';
